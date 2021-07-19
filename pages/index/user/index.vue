5faf4a5fdbf7356fccc470cf28069d4f7b6267eb
Mohsen Pakzad <44529676+mohsenpakzad@users.noreply.github.com>
2021-07-19 17:00:41 +0430

    Create sample for showing joined rooms

--- pages/index/user/index.vue
+++ pages/index/user/index.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
     <p>User Page</p>
-    <v-btn @click="fetchJoinedRooms">fetch Joined Rooms</v-btn>
-    <v-btn @click="fetchRegisteredRooms">fetch Registered Rooms</v-btn>
+<!--    <v-btn @click="fetchJoinedRooms">fetch Joined Rooms</v-btn>-->
+<!--    <v-btn @click="fetchRegisteredRooms">fetch Registered Rooms</v-btn>-->
+    <div v-for="joinedRoom in joinedRooms" :key="joinedRoom.id">
+      <v-btn nuxt link to="room/">{{ joinedRoom.name }}</v-btn>
+    </div>
+    <div v-for="joinedRoom in joinedRooms" :key="joinedRoom.id">
+      <v-btn nuxt link to="room/">{{ joinedRoom.name }}</v-btn>
+    </div>
   </div>
 </template>
 
@@ -11,28 +17,38 @@ import vuexStates from '../../../mixins/vuexStates'
 
 export default {
   name: 'user',
+  data: () => ({
+    joinedRooms: [],
+    registeredRoom: []
+  }),
   mixins: [vuexStates],
-  methods:{
-    async fetchJoinedRooms(){
-      try{
+  methods: {
+    async fetchJoinedRooms() {
+      try {
         const {data} = await this.$axios.get(`/api/users/joined-rooms`, {
           headers: this.$store.getters.tokenHeader
         })
         console.log(data)
-      } catch (e){
+        this.joinedRooms = data
+      } catch (e) {
         console.log(e)
       }
     },
-    async fetchRegisteredRooms(){
-      try{
+    async fetchRegisteredRooms() {
+      try {
         const {data} = await this.$axios.get(`/api/users/registered-rooms`, {
           headers: this.$store.getters.tokenHeader
         })
         console.log(data)
-      } catch (e){
+        this.registeredRoom = data
+      } catch (e) {
         console.log(e)
       }
     },
   },
+  async mounted() {
+    await this.fetchJoinedRooms()
+    await this.fetchRegisteredRooms()
+  }
 }
 </script>
